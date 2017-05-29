056c1d089f19bbfbf158442754fef381a73236b2
Hideo Hattori <hattori-h@klab.com>
2017-05-29 10:57:30 +0900

    strict pep8

--- tests/trial.py
+++ tests/trial.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from __future__ import (absolute_import, division,
-						print_function, unicode_literals)
+                        print_function, unicode_literals)
 import imp
 import os
 import multiprocessing
@@ -9,69 +9,80 @@ name = 'nude'
 fp, pathname, description = imp.find_module(name, ['..'])
 
 try:
-	nude = imp.load_module(name, fp, pathname, description)
+    nude = imp.load_module(name, fp, pathname, description)
 except Exception as e:
-	print(e)
+    print(e)
 finally:
-	if fp:
-		fp.close()
+    if fp:
+        fp.close()
+
 
 def testfile(fname, resize=False):
-		n = nude.Nude(fname)
-		if resize:
-			n.resize(maxheight=800, maxwidth=600)
-		n.parse()
-		return n.result
+    n = nude.Nude(fname)
+    if resize:
+        n.resize(maxheight=800, maxwidth=600)
+    n.parse()
+    return n.result
+
 
 def nudecallback(result):
-	global results
-	if result:
-		results['tp'] += 1
-	else:
-		results['fn'] += 1
-		
+    global results
+    if result:
+        results['tp'] += 1
+    else:
+        results['fn'] += 1
+
+
 def notnudecallback(result):
-	global results
-	if result:
-		results['fp'] += 1
-	else:
-		results['tn'] += 1	
+    global results
+    if result:
+        results['fp'] += 1
+    else:
+        results['tn'] += 1
+
 
 if __name__ == '__main__':
-	import argparse
-	parser = argparse.ArgumentParser(description='Tests the true positive and false positive rate.')
-	parser.add_argument('-r', '--resize', action='store_true', help='Reduce image size to increase speed of scanning')
-	parser.add_argument('-t', '--threads', metavar='int', type=int, required=False, default=0, help='The number of threads to start')
-	args = parser.parse_args()
-	
-	if args.threads < 1:
-		args.threads = None
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Tests the true positive and false positive rate.')
+    parser.add_argument('-r', '--resize', action='store_true',
+                        help='Reduce image size to increase speed of scanning')
+    parser.add_argument('-t', '--threads', metavar='int', type=int,
+                        required=False, default=0, help='The number of threads to start')
+    args = parser.parse_args()
 
-	pool = multiprocessing.Pool(args.threads)
-	results = {}
-	results['tp'] = 0
-	results['tn'] = 0
-	results['fp'] = 0
-	results['fn'] = 0
-	nudepath = os.path.join('samples', 'nude')
-	notnudepath = os.path.join('samples', 'not_nude')
-	for nudepic in os.listdir(nudepath):
-		fname = os.path.join(nudepath, nudepic)
-		pool.apply_async(testfile, (fname, ), {'resize':args.resize}, nudecallback)
-		
-	for notnudepic in os.listdir(notnudepath):
-		fname = os.path.join(notnudepath, notnudepic)
-		pool.apply_async(testfile, (fname, ), {'resize':args.resize}, notnudecallback)
+    if args.threads < 1:
+        args.threads = None
 
-	pool.close()
-	pool.join()
-	nudenum = results['tp'] + results['fn']
-	nonnudenum = results['fp'] + results['tn']
-	if nudenum:
-		print("True positives: ", results['tp'], '/', nudenum, ' - ', (results['tp']/nudenum)*100, '%', sep='')
-	else:
-		print("True positives: N/A")
-	if nonnudenum:
-		print("False positives: ", results['fp'], '/', nonnudenum, ' - ', (results['fp']/nonnudenum)*100, '%', sep='')
-	else:
-		print("False positives: N/A")
+    pool = multiprocessing.Pool(args.threads)
+    results = {}
+    results['tp'] = 0
+    results['tn'] = 0
+    results['fp'] = 0
+    results['fn'] = 0
+    nudepath = os.path.join('samples', 'nude')
+    notnudepath = os.path.join('samples', 'not_nude')
+    for nudepic in os.listdir(nudepath):
+        fname = os.path.join(nudepath, nudepic)
+        pool.apply_async(testfile, (fname, ), {
+                         'resize': args.resize}, nudecallback)
+
+    for notnudepic in os.listdir(notnudepath):
+        fname = os.path.join(notnudepath, notnudepic)
+        pool.apply_async(testfile, (fname, ), {
+                         'resize': args.resize}, notnudecallback)
+
+    pool.close()
+    pool.join()
+    nudenum = results['tp'] + results['fn']
+    nonnudenum = results['fp'] + results['tn']
+    if nudenum:
+        print("True positives: ", results[
+              'tp'], '/', nudenum, ' - ', (results['tp'] / nudenum) * 100, '%', sep='')
+    else:
+        print("True positives: N/A")
+    if nonnudenum:
+        print("False positives: ", results[
+              'fp'], '/', nonnudenum, ' - ', (results['fp'] / nonnudenum) * 100, '%', sep='')
+    else:
+        print("False positives: N/A")
